60afebe7b5d08753050f5ea48a144f4e5c9ad30d
Nicolas Eduardo Ramirez <nramirez@mcti.local>
2019-08-06 10:11:01 -0300

    adding files

--- src/index.js
+++ src/index.js
@@ -2,11 +2,47 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+import allReducers from './reducers';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(allReducers);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+// //ACTION INCREMENT
+// const increment = () => {
+//     return {
+//         type: 'INCREMENT'
+//     }
+// }
+
+// const decrement = () => {
+//     return{
+//         type: 'DECREMENT'
+//     }
+// }
+// //REDUCER
+// const counter = (state = 0, action) => {
+//     switch(action.type){
+//         case 'INCREMENT':
+//             return state + 1;
+//         case 'DECREMENT':
+//             return state - 1;
+//         default:
+//             return state;
+//     }
+// };
+
+// //STORE -> GLOBALIZED STATE
+// let store = createStore(counter);
+// store.subscribe(()=> console.log(store.getState()));
+
+// //DISPATCH
+// store.dispatch(increment());
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.getElementById('root'));
+
